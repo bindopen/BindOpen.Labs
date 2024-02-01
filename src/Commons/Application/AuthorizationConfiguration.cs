6a7e917041c0c3b1070abc576e3ee2412bd6c537
xgonier <xgonier@meltingsoft.com>
2024-02-01 08:09:45 +0100

    - Add authorization configuration.

---
+++ src/Commons/Application/AuthorizationConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BindOpen.Labs.Application
+{
+    /// <summary>
+    /// This class represents the authorization configuration.
+    /// </summary>
+    public class AuthorizationConfiguration
+    {
+        #region Properties
+
+        /// <summary>
+        /// The issuer of this instance.
+        /// </summary>
+        [JsonPropertyName("issuer")]
+        public string Issuer { get; set; }
+
+        /// <summary>
+        /// The audience of this instance.
+        /// </summary>
+        [JsonPropertyName("audience")]
+        public string Audience { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Instantiates a new instance of the AuthorizationConfiguration class.
+        /// </summary>
+        public AuthorizationConfiguration()
+        {
+        }
+
+        #endregion
+    }
+}
